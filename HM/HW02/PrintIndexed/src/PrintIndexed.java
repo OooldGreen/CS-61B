9ea4cc3e5bf92045d91b1ac58fe2acc9258ccbb1
Oooldgreen <9548952242qq.com>
2025-11-05 20:47:57 +0100

    hw01 and hw02 finished

---
+++ HM/HW02/PrintIndexed/src/PrintIndexed.java
@@ -0,0 +1,28 @@
+public class PrintIndexed {
+    /**
+     * Prints each character of a given string followed by the reverse of its index.
+     * Example: printIndexed("hello") -> h4e3l2l1o0
+     */
+    public static void printIndexed(String s) {
+        // TODO: Fill in this function
+        int len = s.length();
+        String str = "";
+
+        // pour l'LLM
+//        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < len; i++) {
+            str += s.charAt(i) + "" +(len-i-1);
+
+            // pour l'LLM
+//            sb.append(s.charAt(i));
+//            sb.append(len-i-1);
+        }
+        System.out.println(str);
+    }
+
+    public static void main(String[] args) {
+        printIndexed("hello");
+        printIndexed("cat"); // should print c2a1t0
+    }
+}
